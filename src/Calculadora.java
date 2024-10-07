dddb03505a9462c359861f0b3eb61c489086edc8
gio10herrera <herrerafontalvo@outlook.com>
2024-10-06 22:34:37 -0500

    declaracion de las variables

---
+++ src/Calculadora.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Calculadora {
+    static JFrame jFrameCalculadora;
+    static JLabel txtLabelX, txtLabelY, txtLabelResult;
+    static JTextField txtFieldX, txtFieldY, txtFieldResult;
+    static JButton btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9, btnPunto, btn0, btnCe, btnSumar, btnRestar, btnResultado, btnMultiplicar, btnDividir, btnC;
+    static int x, y;
+    static JPanel panelBotones = new JPanel(new GridLayout(6, 3, 5, 5));
+    static JPanel panelText = new JPanel(new GridLayout(1, 1, 5, 5));
+
+    public static void main(String[] args) {
+        
+    }
+}
